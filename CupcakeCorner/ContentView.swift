c9d2c33fde252f279013c54c68b023df85a7e9f2
xHadi3 <hadiessam9@gmail.com>
2025-02-09 17:08:51 +0300

    using APIs to get data from the internet

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+
+struct Response: Codable{
+    var results: [Result]
+}
+
+struct Result: Codable{
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId){ item in
+            VStack(alignment: .leading){
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+            
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    func loadData() async{
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do{
+            // data , meta deta or something
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
+                results = decodedResponse.results
+            }
+        }catch{
+            print("Invalid")
+        }
+            
+        
     }
 }
 
